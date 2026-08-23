2b7f485563fa2959926e2a3ea93ca22af757335e
yashanah11 <yashanahsrivastava1@gmail.com>
2026-08-24 04:12:58 +0530

    Add attack taxonomy module

---
+++ attacks/taxonomy.py
@@ -0,0 +1,10 @@
+ATTACK_CATEGORIES = {
+    "social_engineering": [],
+    "identity_abuse": [],
+    "transaction_manipulation": [],
+    "account_takeover": [],
+    "payment_rail_abuse": [],
+    "synthetic_identity": [],
+    "fraud_rings": [],
+    "genai_enabled": [],
+}\n
